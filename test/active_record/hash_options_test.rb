7e3a79f1912c0b8a3628636569a2a7f687ead474
Keenan Brock <keenan@thebrocks.net>
2018-02-07 16:41:23 -0500

    fix like test. add test for compound and not

--- test/active_record/hash_options_test.rb
+++ test/active_record/hash_options_test.rb
@@ -7,7 +7,7 @@ class ActiveRecord::HashOptionsTest < Minitest::Test
     refute_nil ::ActiveRecord::HashOptions::VERSION
   end
 
-  def test_gt_method
+  def test_scope_gt
     Table1.destroy_all
     Table1.create(:name => "small", :value => 1)
     big = Table1.create(:name => "big", :value => 100)
@@ -40,6 +40,14 @@ class ActiveRecord::HashOptionsTest < Minitest::Test
     assert_equal Table1.where(:name => ilike('%big%')), [big]
   end
 
+  def test_ilike_case
+    Table1.destroy_all
+    big1 = Table1.create(:name => "Big", :value => 1)
+    big2 = Table1.create(:name => "big", :value => 100)
+
+    assert_equal Table1.where(:name => ilike('%big%')).order(:name), [big1, big2]
+  end
+
   def test_like
     Table1.destroy_all
     Table1.create(:name => "small", :value => 1)
@@ -56,12 +64,19 @@ class ActiveRecord::HashOptionsTest < Minitest::Test
     assert_equal Table1.where(:name => not_like('%small%')), [big]
   end
 
-  # doesn't work for sqlite
-  # def test_like_case
-  #   Table1.destroy_all
-  #   Table1.create(:name => "Big", :value => 1)
-  #   big = Table1.create(:name => "big", :value => 100)
+  def test_compound_ilike_case
+    Table1.destroy_all
+    big = Table1.create(:name => "Big", :value => 1)
+    Table1.create(:name => "big", :value => 100)
 
-  #   assert_equal Table1.where(:name => ilike('%big%')), [big]
-  # end
+    assert_equal Table1.where(:name => ilike('%big%'), :value => lte(10)), [big]
+  end
+
+  def test_not_compound
+    Table1.destroy_all
+    big = Table1.create(:name => "Big", :value => 1)
+    Table1.create(:name => "big", :value => 100)
+
+    assert_equal Table1.where.not(:name => ilike('%small%'), :value => gte(10)), [big]
+  end
 end
