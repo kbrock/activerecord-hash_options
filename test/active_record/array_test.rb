32baf23b5eb48d6c68eff8631e3fdf058cd74675
Keenan Brock <keenan@thebrocks.net>
2019-09-25 11:33:31 -0400

    introduce case insensitive matching
    it is similar to the ilike case.
    But we tend to downcase and compare strings often.
    This just codifies it.

--- test/active_record/array_test.rb
+++ test/active_record/array_test.rb
@@ -35,6 +35,17 @@ class ActiveRecord::ArrayTest < Minitest::Test
     assert_equal Table1.all.to_a.where(:value => lt(10)), [small]
   end
 
+  # case insensitive tests
+
+  def test_insensitive
+    Table1.destroy_all
+    Table1.create(:name => "small", :value => 1)
+    big = Table1.create(:name => "big", :value => 2)
+    big2 = Table1.create(:name => "BIG", :value => 100)
+
+    assert_equal Table1.all.to_a.where(:name => insensitive('big')).sort_by(&:value), [big, big2]
+  end
+
   # like tests
 
   def test_ilike
