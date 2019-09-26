053bfd103517bcec854d450f89cf8d7d46da6b72
Keenan Brock <keenan@thebrocks.net>
2019-09-25 21:16:38 -0400

    Add table definitions and helpers to specs
    this is duplicate code with the tests for now.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,10 @@
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'bundler/setup'
+Bundler.setup
+
+require 'active_record'
+require 'active_record/hash_options'
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
@@ -15,6 +22,9 @@ RSpec.configure do |config|
     config.default_formatter = "doc"
   end
 
+  # not optimal, but all tests can use the ':column => gt()' syntax
+  config.include ActiveRecord::HashOptions::Helpers
+
   #config.profile_examples = 10
 
   # Run specs in random order to surface order dependencies. If you find an
@@ -29,3 +39,37 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 end
+
+Dir["./spec/support/**/*.rb"].each {|f| require f}
+
+
+class MyTestDatabase
+  def self.setup
+    ActiveRecord::Base.establish_connection adapter: "sqlite3", database: ":memory:"
+  end
+end
+
+MyTestDatabase.setup
+ActiveRecord::Migration.new.create_table :table1s do |t|
+  t.string  :name
+  t.integer :value
+end
+
+class Table1 < ActiveRecord::Base
+  extend ActiveRecord::HashOptions
+  include ActiveRecord::HashOptions::Helpers
+  extend ActiveRecord::HashOptions::Helpers
+
+  def self.big_values
+    where(:value => gt(10))
+  end
+
+  def self.big_name
+    where(:name => like("%big%"))
+  end
+
+  def self.big_iname
+    where(:name => ilike("%big%"))
+  end
+end
+
