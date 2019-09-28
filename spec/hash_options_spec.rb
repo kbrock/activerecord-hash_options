62f9ba7c44b1f9dd9fa84cada656a77c07c7fc34
Keenan Brock <keenan@thebrocks.net>
2019-09-28 01:42:32 -0400

    update tests for case sensitivity
    ranges, and comparisons yield different values for all platforms
    
    frustrated and concerned about the sheer number of use cases.
    
    trying to figure out how to ensure that ruby and the database act
    the same vs ruby always consistently acting the same way.

--- spec/hash_options_spec.rb
+++ spec/hash_options_spec.rb
@@ -48,7 +48,11 @@ RSpec.describe ActiveRecord::HashOptions do
 
   shared_examples "string comparable" do
     it "compares with gt" do
-      expect(filter(collection, :name => gt("big"))).to eq([small])
+      if !mac? && pg?
+        expect(filter(collection, :name => gt("big"))).to eq([small, big2])
+      else
+        expect(filter(collection, :name => gt("big"))).to eq([small])
+      end
     end
 
     it "compares with gte" do
@@ -60,12 +64,15 @@ RSpec.describe ActiveRecord::HashOptions do
     end
 
     it "compares with lte" do
-      expect(filter(collection, :name => lte("big"))).to match_array([big, big2])
+      if !mac? && pg?
+        expect(filter(collection, :name => lte("big"))).to eq([big])
+      else
+        expect(filter(collection, :name => lte("big"))).to match_array([big, big2])
+      end
     end
 
     it "compares with range" do
-      # sqlite3 is case sensitive for ranges but not for other string operations
-      if case_sensitive? || ENV["DB"] == "sqlite3"
+      if array_test? || (pg? && mac?) || sqlite?
         expect(filter(collection, :name => "big"..."small")).to eq([big])
         expect(filter(collection, :name => "big".."small")).to match_array([big, small])
       else
@@ -87,7 +94,7 @@ RSpec.describe ActiveRecord::HashOptions do
     end
 
     it "compares with like" do
-      if case_sensitive?
+      if array_test? || pg?
         expect(filter(collection, :name => like('%big%'))).to eq([big])
       else
         expect(filter(collection, :name => like('%big%'))).to eq([big, big2])
@@ -99,7 +106,7 @@ RSpec.describe ActiveRecord::HashOptions do
     end
 
     it "compares with starts_with" do
-      if case_sensitive?
+      if array_test? || pg?
         expect(filter(collection, :name => starts_with('b'))).to eq([big])
       else
         expect(filter(collection, :name => starts_with('b'))).to eq([big, big2])
@@ -107,7 +114,7 @@ RSpec.describe ActiveRecord::HashOptions do
     end
 
     it "compares with ends_with" do
-      if case_sensitive?
+      if array_test? || pg?
         expect(filter(collection, :name => ends_with('g'))).to eq([big])
       else
         expect(filter(collection, :name => ends_with('g'))).to eq([big, big2])
@@ -115,7 +122,7 @@ RSpec.describe ActiveRecord::HashOptions do
     end
 
     it "compares with contains" do
-      if case_sensitive?
+      if array_test? || pg?
         expect(filter(collection, :name => contains('i'))).to match_array([big])
       else
         expect(filter(collection, :name => contains('i'))).to match_array([big, big2])
@@ -127,7 +134,7 @@ RSpec.describe ActiveRecord::HashOptions do
 
   shared_examples "regexp comparable" do
     it "compares with regexp" do
-      skip("db #{ENV["db"]} does not support regexps") unless supports_regexp?
+      skip("db #{ENV["db"]} does not support regexps") unless array_test? || pg?
 
       expect(filter(collection, :name => /^bi.*/)).to eq([big])
       expect(filter(collection, :name => /^Bi.*/)).to eq([])
@@ -169,18 +176,20 @@ RSpec.describe ActiveRecord::HashOptions do
 
   private
 
-  # pg and arrays support regular expressions
-  def supports_regexp?
-    array_test? || ENV["DB"] == "pg"
-  end
-
-  # sqlite is not case sensitive, pg on ubunto is not case sensitive, pg on mac is case sensitive
-  def case_sensitive?
-    array_test?
+  def mac?
+    Gem::Platform.local.os == "darwin"
   end
 
   def array_test?
     collection.kind_of?(Array)
+  end
+
+  def pg?
+    (ENV["DB"] == "pg" && !array_test?)
+  end
+
+  def sqlite?
+    ENV["DB"] == "sqlite3" && !array_test?
   end
 
   def filter(collection, conditions, negate = false)
